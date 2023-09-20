eba503b2538981debff671cca3406cbeb7b142ed
Aurelia H <aurelia.h@hotmail.fr>
2023-09-20 16:18:30 +0200

    change

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'>
     <div>
-      <Link href={"/movieTrackersApp"}>Appli Trackers de FILMS</Link>
+      <Link href={"/movieTrackersApp"}>Appli Trackers de FILMS !!!</Link>
     </div>
     <div>
       <Link href={"/TestApiAxiosEtFetch"}>TestApiAxiosEtFetch</Link>
